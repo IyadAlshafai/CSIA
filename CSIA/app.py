592e27166de9ab632c18e018b50caf71a3e8e41a
doood5555 <iyad.alshafai@thekaustschool.org>
2025-10-06 10:04:27 +0300

    Finished most of the frontend only the backend is left

--- CSIA/app.py
+++ CSIA/app.py
@@ -1,30 +1,29 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
+import pymysql
 
-app = Flask(__name__)
+cnx = ""
+try:
+  cnx = pymysql.connect (
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    database = 'Feedback_Database',
+    port=8889,
+  )
+except pymysql.Error as Er:
+  print(Er)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/Feedback_Database'
-config = {
-  'user': 'root',
-  'password': 'root',
-  'host': '127.0.0.1',
-  'port': 8889,
-  'database': 'feedback_database',
-  'raise_on_warnings': True
-}
+cursor = cnx.cursor()
 
-cnx = mysql.connector.connect(**config)
+cursor.execute('SELECT * FROM `Feedback`')
 
-cursor = cnx.cursor(dictionary=True)
-
-cursor.execute('SELECT `User_id`, `name` FROM `Users`')
 results = cursor.fetchall()
 
+columns = [desc[0] for desc in cursor.description]
+
 for row in results:
-  id = row['User_id']
-  title = row['name']
-  print('%s | %s' % (id, title))
+    for col, val in zip(columns, row):
+        print(f"{col}: {val}")
+    print("-" * 40)
 
-
-db = SQLAlchemy(app)
+cnx.close()
